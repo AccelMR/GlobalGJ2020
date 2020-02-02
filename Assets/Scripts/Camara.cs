9bac6195cc50a08128e78977dee823874ba24856
arlo99 <idv19c.alopez@uartesdigitales.edu.mx>
2020-02-01 21:00:35 -0600

    camara script

---
+++ Assets/Scripts/Camara.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    [SerializeField]
+    int m_limiteMax=20;
+    [SerializeField]
+    int m_limiteMin = 0;
+    private float m_speed =1.0f;
+    public GameObject ship;
+    // Start is called before the first frame update
+  Vector3 offset;
+    void Start()
+    {
+       // offset = transform.position - ship.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mov = Input.GetAxisRaw("Vertical");
+        if (mov != 0) {
+
+        transform.position += new Vector3(0,0, mov) * m_speed *Time.fixedDeltaTime;
+
+        }
+        
+        //Debug.Log("si sirve weee");
+        //transform.position = ship.transform.position + offset;
+    }
+}
