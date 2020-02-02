0f49e76a5107ccdba5f9af80134451994c910e12
arlo99 <idv19c.alopez@uartesdigitales.edu.mx>
2020-02-01 23:29:46 -0600

    no message

---
+++ Assets/Scripts/load_screen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class load_screen : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        yield return  new WaitForSeconds(4.0f);
+        SceneManager.GetSceneByName("GameScene");
+
+    }
+    
+}
