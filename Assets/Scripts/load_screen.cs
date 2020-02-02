05fd0ee42786ecedde71b161315d7d2317667e97
arlo99 <idv19c.alopez@uartesdigitales.edu.mx>
2020-02-02 02:28:01 -0600

    load screen

--- Assets/Scripts/load_screen.cs
+++ Assets/Scripts/load_screen.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class load_screen : MonoBehaviour
 {
-    private IEnumerator Start()
-    {
-        yield return  new WaitForSeconds(4.0f);
-        SceneManager.GetSceneByName("GameScene");
+    public GameObject loadscreen;
+    public Slider valor;
+    public void LoadLevel(int index) {
+
+        StartCoroutine(LoadAsynchronously(index));
+    }
+
+    IEnumerator LoadAsynchronously(int index) {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(index);
+        loadscreen.SetActive(true);
+        while (!operation.isDone) {
+            float progress = Mathf.Clamp01(operation.progress / 1f);
+            valor.value = progress;
+            yield return null;
+        }
 
     }
     
